21f857a644b0dd7b04915d37b129762cec9d1a72
Eraz19 <alexanderdouieb19@gmail.com>
2025-01-29 19:13:00 +0100

    modified vscode installation software and extension permissions

--- install.sh
+++ install.sh
@@ -75,10 +75,10 @@ function InstallVsCode()
     {
         echo "Installing VsCode...";
 
-        sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg;
+        sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg;
         sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/;
-        sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list';
-        sudo apt install -y code;
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list;
+        sudo apt update && sudo apt install -y code;
     };
 
     function InstallExtensions()
@@ -115,7 +115,7 @@ function InstallVsCode()
 
         for ext in "${extensions[@]}";
         do
-            sudo code --install-extension $ext;
+            code --install-extension $ext;
         done
     };
 
