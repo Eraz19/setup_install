fed053501a72dee3a7f3c39ab9d4a87db2efc9e5
Eraz19 <alexanderdouieb19@gmail.com>
2025-01-31 10:32:11 +0100

    fix infinite loop in KillingLoginWindow

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ function InstallDiscord()
     sudo dpkg -i discord.deb;
     sudo apt install -f -y;
 
-    setsid discord & KillDiscordOnLoginWindow;
+    setsid discord &> /dev/null & KillDiscordOnLoginWindow;
 };
 
 function InstallVsCode()
