d21c32882fd5b64f325e056d8a32cb032776bbc6
Eraz19 <alexanderdouieb19@gmail.com>
2025-02-05 17:24:55 +0100

    fixed oh-my-posh custom config in .zshrc file import

--- install.sh
+++ install.sh
@@ -621,7 +621,7 @@ function InstallTerminalUtilities()
 
         function SettingOhMyPoshLaunching()
         {
-            SetZshConfigFile "eval '\$(oh-my-posh init zsh --config $PROJECT_ROOT_FOLDER/oh_my_posh/custom.omp.json)'" 0;
+            SetZshConfigFile "eval "\$(oh-my-posh init zsh --config $PROJECT_ROOT_FOLDER/oh_my_posh/custom.omp.json)"" 0;
         };
 
         echo "Installing Oh-My-Posh...";
@@ -629,8 +629,8 @@ function InstallTerminalUtilities()
         sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh;
         sudo chmod +x /usr/local/bin/oh-my-posh;
         SettingOhMyPoshLaunching;
-        InstallNerdFont;
-        ChangeGnomeTerminalFont "$FONT_NAME" "$FONT_SIZE";
+        #InstallNerdFont;
+        #ChangeGnomeTerminalFont "$FONT_NAME" "$FONT_SIZE";
     };
 
     function ChangeDefaultShellToZsh()
