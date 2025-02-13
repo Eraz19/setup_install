182d1be4cabd440351b65e21a64dab4118257ae0
Eraz19 <alexanderdouieb19@gmail.com>
2025-02-13 18:15:40 +0100

    fix installation sptify, retroarch

--- install.sh
+++ install.sh
@@ -398,7 +398,7 @@ function InstallApps()
         };
 
         AddSpotifyRepository;
-        sudo apt install -y spotify-client;
+        sudo apt update && sudo apt install -y spotify-client;
     };
 
     #InstallSteam          ;
@@ -1247,15 +1247,10 @@ then
     #InstallGnomeUIUtilities  ;
     InstallApps              ;
     #InstallCodingEcosystem   ;
-    ConfigSystemSettings     ;
+    #ConfigSystemSettings     ;
     #InstallTerminalUtilities ;
 
     RemoveIncreaseSudoEffectiveness;
 
     #gnome-session-quit --logout --no-prompt;
 fi
-
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
-sudo apt-get update && sudo apt-get install spotify-client
