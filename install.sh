4eea3edd458cd9932e75d535acd8762a2d65ff49
Eraz19 <alexanderdouieb19@gmail.com>
2025-02-13 12:46:28 +0100

    fix yazi instalation

--- install.sh
+++ install.sh
@@ -553,7 +553,7 @@ function InstallCodingEcosystem()
                 source $HOME/.cargo;
             };
 
-            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y;
             rustup update;
             ManuallySourceNvm;
         };
