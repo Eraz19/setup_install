8eed38087d9ef2f7ba24eb31dd8749083da1749b
Eraz19 <alexanderdouieb19@gmail.com>
2025-02-14 13:14:02 +0100

    automatize proton enabled in steam

--- install.sh
+++ install.sh
@@ -149,56 +149,6 @@ function InstallGnomeUIUtilities()
 
 function InstallApps()
 {
-#    function InstallSteam()
-#    {
-#        function InstallSoftware()
-#        {
-#            function AvoidingInteractionPrompt()
-#            {
-#                echo steam steam/license boolean true      | sudo debconf-set-selections ;
-#                echo steam steam/question select "I AGREE" | sudo debconf-set-selections ;
-#            };
-#
-#            sudo add-apt-repository -y multiverse;
-#            sudo apt update;
-#
-#            AvoidingInteractionPrompt;
-#            sudo DEBIAN_FRONTEND=noninteractive apt install -y steam steamcmd;
-#        };
-#
-#        function LaunchFirstUpdate()
-#        {
-#            function KillSteamOnLoginWindow()
-#            {
-#                while true;
-#                do
-#                    if pgrep -f steam-runtime-launcher-service >/dev/null;
-#                    then
-#                        pkill -f steam;
-#                        break;
-#                    fi
-#
-#                    sleep 2;
-#                done
-#            };
-#
-#            nohup steam steam://open/install &> /dev/null & KillSteamOnLoginWindow;
-#        };
-#
-#        function InstallSteamDownloads()
-#        {
-#            steamcmd +login "$STEAM_USERNAME" "$STEAM_PASSWORD" \
-#                     +app_update 1493710 validate \   # Proton 9.0
-#                     +app_update 1145360 validate \   # Hades
-#                     +quit
-#        };
-#
-#        echo "Installing Steam...";
-#
-#        InstallSoftware       ;
-#        LaunchFirstUpdate     ;
-#        InstallSteamDownloads ;
-#    };
 
     function InstallSteam()
     {
@@ -238,6 +188,29 @@ function InstallApps()
 
         function InstallSteamDownloads()
         {
+            function ApplyProton()
+            {
+                local user_id=$(find ~/.steam/steam/userdata/ -maxdepth 1 -type d -exec basename {} \; | grep -E '^[0-9]+$');
+                local config_file="$HOME/.steam/steam/userdata/$user_id/config/config.vdf";
+
+                if [ ! -d "$(dirname "$config_file")" ];
+                then
+                    echo "Steam config directory not found. Exiting."
+                    exit 1
+                fi
+
+                if [ -f "$config_file" ];
+                then
+                    cp "$config_file" "$config_file.bak"
+                    echo "Backup created: $config_file.bak"
+                fi
+
+                awk 'BEGIN {print "{\n\"CompatToolMapping\": {\n\"1145360\": {\n\"name\": \"proton_90\",\n\"config\": \"\",\n\"priority\": 250\n}\n}\n}"}' > "$config_file"
+
+                echo "Proton compatibility settings applied for Hades (App ID 1145360)."
+                steam --restart   
+            };
+
             steamcmd +login "$STEAM_USERNAME" "$STEAM_PASSWORD" \
                     +app_update 1493710 validate \
                     +quit;
@@ -246,6 +219,9 @@ function InstallApps()
                     +@sSteamCmdForcePlatformType windows \
                     +app_update 1145360 validate \
                     +quit;
+
+            # Apply Proton compatibility settings
+            ApplyProton;
         };
 
         echo "Installing Steam...";
@@ -254,6 +230,63 @@ function InstallApps()
         LaunchFirstUpdate     ;
         InstallSteamDownloads ;
     };
+
+
+
+#    function InstallSteam()
+#    {
+#        function InstallSoftware()
+#        {
+#            function AvoidingInteractionPrompt()
+#            {
+#                echo steam steam/license boolean true      | sudo debconf-set-selections ;
+#                echo steam steam/question select "I AGREE" | sudo debconf-set-selections ;
+#            };
+#
+#            sudo add-apt-repository -y multiverse;
+#            sudo apt update;
+#
+#            AvoidingInteractionPrompt;
+#            sudo DEBIAN_FRONTEND=noninteractive apt install -y steam steamcmd;
+#        };
+#
+#        function LaunchFirstUpdate()
+#        {
+#            function KillSteamOnLoginWindow()
+#            {
+#                while true;
+#                do
+#                    if pgrep -f steam-runtime-launcher-service >/dev/null;
+#                    then
+#                        pkill -f steam;
+#                        break;
+#                    fi
+#
+#                    sleep 2;
+#                done
+#            };
+#
+#            nohup steam steam://open/install &> /dev/null & KillSteamOnLoginWindow;
+#        };
+#
+#        function InstallSteamDownloads()
+#        {
+#            steamcmd +login "$STEAM_USERNAME" "$STEAM_PASSWORD" \
+#                    +app_update 1493710 validate \
+#                    +quit;
+#
+#            steamcmd +login "$STEAM_USERNAME" "$STEAM_PASSWORD" \
+#                    +@sSteamCmdForcePlatformType windows \
+#                    +app_update 1145360 validate \
+#                    +quit;
+#        };
+#
+#        echo "Installing Steam...";
+#
+#        InstallSoftware       ;
+#        LaunchFirstUpdate     ;
+#        InstallSteamDownloads ;
+#    };
 
     function InstallDiscord()
     {
